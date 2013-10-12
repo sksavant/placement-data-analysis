aad7f06acd3fa85ccf15af6c1a5c1496f195c473
Savant Krishna <savant.2020@gmail.com>
2013-10-12 13:36:56 +0530

    completed class structre

--- src/saveData.py
+++ src/saveData.py
@@ -11,22 +11,22 @@ placement_jafs_page = 'http://placements.iitb.ac.in/placements/studjaf4studnew.j
 
 class PlacementsWeb:
     def __init__(self):
-        auth = Authentication()
-        assert (auth.username == "sk.savant")
+        self.auth = Authentication()
+        assert (self.auth.username == "sk.savant")
 
-        br = mechanize.Browser()
-        cj = cookielib.LWPCookieJar()
-        br.set_cookiejar(cj)
+        self.br = mechanize.Browser()
+        self.cj = cookielib.LWPCookieJar()
+        self.br.set_cookiejar(self.cj)
 
         # Options
-        br.set_handle_equiv(True)
+        self.br.set_handle_equiv(True)
         #br.set_handle_gzip(True)
-        br.set_handle_redirect(True)
-        br.set_handle_referer(True)
-        br.set_handle_robots(False)
+        self.br.set_handle_redirect(True)
+        self.br.set_handle_referer(True)
+        self.br.set_handle_robots(False)
 
         # Handling refresh
-        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        self.br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
         # Debug messages
         #br.set_debug_http(True)
@@ -34,45 +34,45 @@ class PlacementsWeb:
         #br.set_debug_responses(True)
 
         # User agent
-        br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-        br.set_proxies({})
+        self.br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+        self.br.set_proxies({})
 
     def login(self):
         print placement_login_page
-        r = br.open(placement_login_page)
+        r = self.br.open(placement_login_page)
         html = r.read()
 
         n_forms = 0
 
-        for f in br.forms():
+        for f in self.br.forms():
             n_forms = n_forms+1
 
         assert (n_forms == 1)
 
-        br.select_form(nr=0)
+        self.br.select_form(nr=0)
 
-        br.form['ldaplogin'] = auth.username
-        br.form['ldappasswd'] = auth.password
-        br.submit()
+        self.br.form['ldaplogin'] = self.auth.username
+        self.br.form['ldappasswd'] = self.auth.password
+        self.br.submit()
 
-        assert br.geturl() == placement_home
+        assert self.br.geturl() == placement_home
 
     def getJAFs(self):
 
-        br.find_link(text='View / Sign JAFs')
-        req = br.click_link(text='View / Sign JAFs')
-        br.open(req)
+        self.br.find_link(text='View / Sign JAFs')
+        req = self.br.click_link(text='View / Sign JAFs')
+        self.br.open(req)
         #print br.response().read()
-        assert br.geturl() == placement_jafs_page
+        assert self.br.geturl() == placement_jafs_page
         no_of_jafs = 0
-        for l in br.links(url_regex=re.compile('studjafview')):
+        for l in self.br.links(url_regex=re.compile('studjafview')):
             no_of_jafs = no_of_jafs+1
-            getJAFInfo(l)
+            self.getJAFInfo(l)
 
         print no_of_jafs
 
     def getJAFInfo(self,l):
-        print l
+        print 'Getting JAF Info for',l.text,":",l.url
 
 if __name__=='__main__':
     p = PlacementsWeb()
