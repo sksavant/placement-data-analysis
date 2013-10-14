dade254c95b3efb38c6404fd848f3d289bed688b
Savant Krishna <savant.2020@gmail.com>
2013-10-14 17:52:35 +0530

    JAFSaver class which saves html files from the web

--- src/saveData.py
+++ src/saveData.py
@@ -4,37 +4,49 @@ from JAF import JAF
 from placementsWeb import PlacementsWeb
 from JAFReader import JAFReader
 
-def getAllJAFs():
-    p = PlacementsWeb()
-    p.login()
-    for l in p.getJAFLinks():
-        cn,jn,html = p.getJAFPage(l)
-        j = JAF(cn,jn)
-        jr = JAFReader(html,j)
-        jr.parseInfo()
-        print j.getJAFString()
-        break
-
 def writeJAFtoFile(cn, jn, html):
     f = open("../data/"+cn+str(jn)+".html",'w')
     f.write(html)
     f.close()
 
-def saveAllHTML():
-    p = PlacementsWeb()
-    p.login()
-    for l in p.getJAFLinks():
-        cn,jn,html = p.getJAFPage(l)
-        writeJAFtoFile(cn,jn,html)
-        print cn,jn
+class JAFSaver:
+    def __init__(self):
+        self.p = PlacementsWeb()
+        self.p.login()
+        self.linklist = self.p.getJAFLinks()
 
-def printJAFHTML(compname, jafno):
-    p = PlacementsWeb()
-    p.login()
-    cn,jn,html = p.getCompJAFPage(compname, jafno)
-    writeJAFtoFile(cn,jn,html)
+    def getAllJAFs(self):
+        for l in linklist:
+            cn,jn,html = self.p.getJAFPage(l)
+            j = JAF(cn,jn)
+            jr = JAFReader(html,j)
+            jr.parseInfo()
+            print j.getJAFString()
+            break
+
+    def saveAllHTML(self):
+        for l in linklist:
+            cn,jn,html = p.getJAFPage(l)
+            writeJAFtoFile(cn,jn,html)
+            print cn,jn
+
+    def printJAFHTML(self,compname, jafno):
+        cn,jn,html = self.p.getCompJAFPage(compname, jafno)
+        writeJAFtoFile(cn,jn,html)
+
+    def printMissingJAFs(self):
+        f = open('../data/missing-comp.txt','r')
+        while True:
+            x = f.readline().split()
+            if x==[]:
+                break
+            cn = x[0]
+            jn = int(x[1])
+            #print cn,jn
+            self.printJAFHTML(cn,jn)
 
 if __name__=='__main__':
     #getAllJAFs()
     #saveAllHTML()
-    printJAFHTML("walmart",1)
+    s = JAFSaver()
+    s.printMissingJAFs()
